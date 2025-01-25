9c7243b086da3baec8c5b9ed01cdc29b7d79f708
antilpiyush89 <55829334+antilpiyush89@users.noreply.github.com>
2025-01-25 22:46:35 +0530

    target added

--- Flashgram-Frontend/src/App.tsx
+++ Flashgram-Frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { GridBackgroundDemo } from "./components/Background"
 import { Navbar } from "./components/Navbar"
 
+// Target -> add flashcards -> Ques Ans usmein
+// Make it flippable to reveal ans
+// Make it scrollable -> infinite scroll and then optimise
 function App() {
   return <div>
     < GridBackgroundDemo />
